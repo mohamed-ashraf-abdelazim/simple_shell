2b20069b55cae2a8111ef536b3cfe4fc62e10911
muhammedbalogun <muhammed.balogun123@gmail.com>
2024-01-10 04:34:59 +0000

    updated the_executer function

--- the_executer.c
+++ the_executer.c
@@ -14,6 +14,7 @@ void execute_the_command(const char *the_command)
 		the_print_func("Error forking process.\n");
 		exit(EXIT_FAILURE);
 	}
+
 	else if (chld_pid == 0)
 	{
 		char *the_args[128];
@@ -21,15 +22,17 @@ void execute_the_command(const char *the_command)
 		char *the_token = strtok((char *)the_command, " ");
 
 		while (the_token != NULL)
-	{
-		the_args[args_count++] = the_token;
-		the_token = strtok(NULL, " ");
-	}
+		{
+			the_args[args_count++] = the_token;
+			the_token = strtok(NULL, " ");
+		}
+
 		the_args[args_count] = NULL;
 		execvp(the_args[0], the_args);
 		the_print_func("Error executing command.\n");
 		exit(EXIT_FAILURE);
 	}
+
 	else
 	{
 		wait(NULL);
